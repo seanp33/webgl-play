0967588fffe2ec5bf92f2ae6ff3d2ae01c387c9f
Sean Monaghan <smonagan000@gmail.com>
2013-12-12 23:54:38 -0500

    pseudo dynamic point cloud by repositioning particles

--- src/js/splotch/ui/ParticleGraph.js
+++ src/js/splotch/ui/ParticleGraph.js
@@ -7,6 +7,7 @@ define(
             function ParticleGraph(scene){
                 this.scene = scene;
                 this.particles = new THREE.Geometry(),
+                //this.particles.dynamic = true;
                 this.pMaterial = new THREE.ParticleBasicMaterial({
                       color: 0xFF0000,
                       size: 10
@@ -18,7 +19,11 @@ define(
                 init:function(){
                     console.log('initializing');
                     
-                    
+                    this.particleSystem = new THREE.ParticleSystem(this.particles, this.pMaterial);
+                    this.particleSystem.sortParticles = true;
+                    this.particleSystem.geometry.dynamic = true;
+                    this.scene.add(this.particleSystem);                    
+                    this._fill_system(100000);
                     var self = this;
                     clock.add(function(){
                         self.update();
@@ -26,17 +31,27 @@ define(
                 },
                 
                 add_nodes:function(nodes){
-                    this.particleSystem = new THREE.ParticleSystem(this.particles, this.pMaterial);
-                    this.scene.add(this.particleSystem);
                     var particle = null;
-                    for(var i=0; i < nodes.length; i++){
-                       var n = nodes[i];
-                       particle = new THREE.Vertex(new THREE.Vector3(n.pos.x, n.pos.y, n.pos.z));
-                       this.particles.vertices.push(particle);
-                   }
+                    var start = this.last_node_count || 0;
+                    var end = nodes.length+start;
+                    console.log(start + ' to ' + end);
+                    for(var i=start; i < end; i++){
+                        var n = nodes[i-start];
+                        var p = this.particles.vertices[i];
+                        p.set(n.pos.x, n.pos.y, n.pos.z);
+                    }
+                    
+                    this.last_node_count = end;
                 },
                 
                 update:function(){},
+                
+                _fill_system:function(size){
+                    for(var i=0; i < size; i++){
+                       particle = new THREE.Vector3(0, 0, 0);
+                       this.particles.vertices.push(particle);
+                    }
+                }
                 
             }
             
